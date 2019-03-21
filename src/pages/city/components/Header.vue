4d50b7eda48b434350e44144d63a945873ff8552
阿怪 <862704559@qq.com>
2019-03-21 09:54:47 +0800

    add city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <router-link
+      to="/"
+      tag="div"
+      class="header-left"
+    >
+        <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+    <div class="header-title">
+      城市
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
+    position: relation
+    .header-left
+      width: .64rem
+      position: absolute
+      top: 0
+      left: 0
+      .back-icon
+        font-size: .24rem
+        text-align: center
+    .header-title
+      width: 100%
+      height: $headerHeight
+      line-height: $headerHeight
+      text-align: center
+      color:#fff;
+      font-size: .32rem
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      城市
-      <span class="iconfont font-icon">&#xe64a;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        城市
+        <span class="iconfont font-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -50,6 +52,7 @@ export default {
       color:#ccc;
       border-radius: .1rem
     .header-right
+      color: #fff
       width: 1.24rem
       float: right
       text-align: center
